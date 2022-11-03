2e838db198a6663e5eaac995d5131e321c99a1f6
tesol2y090 <gliese090@gmail.com>
2022-11-03 07:24:30 +0700

    add: upload data to ipfs

--- chainlink-adapter/src/index.js
+++ chainlink-adapter/src/index.js
@@ -1,8 +1,8 @@
-const app = require('./app');
-require('dotenv').config();
+const app = require("./app")
+require("dotenv").config()
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080
 
 app.listen(PORT, () => {
-    console.log(`Now listening on http://localhost:${PORT}`);
-});
+  console.log(`Now listening on http://localhost:${PORT}`)
+})
--- chainlink-adapter/src/services/getCompoundCredits.js
+++ chainlink-adapter/src/services/getCompoundCredits.js
@@ -1,4 +1,8 @@
 const { gql, request } = require("graphql-request")
+const { Web3Storage, File } = require("web3.storage")
+require("dotenv").config()
+
+const web3ApiKey = process.env.WEB3_STORAGE_API
 
 const endpoint =
   "https://api.thegraph.com/subgraphs/name/graphprotocol/compound-v2"
@@ -16,6 +20,8 @@ const query = gql`
     }
   }
 `
+
+const storage = new Web3Storage({ token: web3ApiKey })
 
 module.exports = async function getCompoundCredits(req, res) {
   if (!req.body.data) {
@@ -37,13 +43,14 @@ module.exports = async function getCompoundCredits(req, res) {
     borrowerAccount: account,
   }
 
-  console.log("variables", variables)
-
   try {
-    const data = await request(endpoint, query, variables)
-    console.log(data)
+    const res = await request(endpoint, query, variables)
+    const buffer = Buffer.from(JSON.stringify(res))
+    const dataFile = [new File([buffer], `${account}-compound-credits.json`)]
+    const cid = await storage.put(dataFile)
+    console.log("stored files with cid:", cid)
     return res.status(200).json({
-      credits: data,
+      credits: cid,
     })
   } catch (err) {
     console.log(err)
